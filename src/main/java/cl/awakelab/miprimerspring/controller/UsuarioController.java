d9af2d128ade5478d40b1a6534adfa1e169d83bf
Gabrielcorderomutinelli@gmail.com <gabrielcorderomutinelli@gmail-com>
2023-09-06 21:58:07 -0300

    Actualizaacion proyecto

--- src/main/java/cl/awakelab/miprimerspring/controller/UsuarioController.java
+++ src/main/java/cl/awakelab/miprimerspring/controller/UsuarioController.java
@@ -5,7 +5,7 @@ import cl.awakelab.miprimerspring.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +16,28 @@ public class UsuarioController {
     @Autowired
     IUsuarioService objUsuarioService;
 
+    @GetMapping("/crearUsuario") // Va  a buscar al formulario Html
+    public String mostrarFormularioCrearUsuario(){
+        return "templateFormularioCrearUsuario";
+    }
+
+    @PostMapping("/crearUsuario")
+    public String crearUsuario(@ModelAttribute Usuario usuario){
+        objUsuarioService.crearUsuario(usuario);
+        return "redirect:/Usuario";
+
+    }
+
+    @GetMapping
     public String listarUsuario(Model model){
         List<Usuario> listaUsuarios = objUsuarioService.listarUsuario();
         model.addAttribute("atributoListaUsuarios", listaUsuarios);// Le pasamos la lista de usuarios al objeto atributoListaUsuarios
         return "templateListarUsuarios";
     }
 
+    @PostMapping("/eliminar/{id}")
+    public String eliminarUsuario(@PathVariable int id){
+        objUsuarioService.eliminarUsuario(id);
+        return "redirect:/Usuario";
+    }
 }
